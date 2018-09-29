4d63aa4a40f8999091d2673b632e6af674707480
Pwnulatr <12719642+Pwnulatr@users.noreply.github.com>
2018-09-28 23:27:50 -0400

    Begin refactoring (#4)
    Refactor outagetally

--- outagetally/outagetally.py
+++ outagetally/outagetally.py
@@ -5,7 +5,10 @@ from discord.ext import commands
 from .utils.dataIO import dataIO
 
 __author__ = "Pwnulatr"
-__version__ = "2.1.1"
+__version__ = "2.1.2"
+
+PATH = "data/pwning-cogs/outagecounter/"
+JSON = PATH + "tally.json"
 
 
 class Outagetally:
@@ -13,53 +16,38 @@ class Outagetally:
 
     def __init__(self, bot):
         self.bot = bot
-        self.file_path = "data/pwning-cogs/outagecounter/tally.json"
-        self.settings = dataIO.load_json(self.file_path)
+        self.settings = dataIO.load_json(JSON)
 
     @commands.command(no_pm=False, pass_context=True)
     async def internetwentout(self, ctx):
         """Just adds one to the tally"""
 
-        user = ctx.message.author.id
-        date_today = date.today()
         await self.bot.type()
-        if user not in self.settings:
-            self.settings[user] = {}
-            self.settings[user]["Tally"] = 1
-            self.settings[user]["Last Outage"] = f"{date_today}"
-            dataIO.save_json(self.file_path, self.settings)
+        if ctx.message.author.id not in self.settings:
+            self.settings[ctx.message.author.id] = {"Tally": 1, "Last Outage": date.today()}
+            dataIO.save_json(JSON, self.settings)
             await self.bot.reply("This appears to be your first outage on record.")
-        elif user in self.settings:
-            current_tally = self.settings[user]['Tally']
-            add_to_tally = current_tally + 1
-            self.settings[user]["Tally"] = add_to_tally
-            self.settings[user]["Last Outage"] = f"{date_today}"
-            dataIO.save_json(self.file_path, self.settings)
-            await self.bot.reply(f"Logged! You're currently at {add_to_tally} outages!")
-        else:
-            await self.bot.reply("Something went wrong.")
+        elif ctx.message.author.id in self.settings:
+            auth_id = self.settings[ctx.message.author.id]
+            auth_id["Tally"] = auth_id['Tally'] + 1
+            auth_id["Last Outage"] = date.today()
+            dataIO.save_json(JSON, self.settings)
+            await self.bot.reply(f"Logged! You're currently at {auth_id['Tally'] + 1} outages!")
 
     @commands.command(no_pm=False, pass_context=True)
     async def howmanytimes(self, ctx, *, user: discord.Member = None):
         """Says what the current tally is"""
 
-        author = ctx.message.author
-
         if not user:
-            user = author
+            user = ctx.message.author
 
-        user_id = user.id
-
-        if user_id in self.settings:
-            tally_count = self.settings[user_id]["Tally"]
-            last_out = self.settings[user_id]["Last Outage"]
+        if user.id in self.settings:
+            auth_id = self.settings[user.id]
             info = discord.Embed(colour=user.colour)
-            info.add_field(name="Total Disconnects", value=tally_count)
-            info.add_field(name="Last Outage", value=last_out)
+            info.add_field(name="Total Disconnects", value=auth_id["Tally"])
+            info.add_field(name="Last Outage", value=auth_id["Last Outage"])
 
-            name = str(user)
-            name = " | ".join((name, user.nick)) if user.nick else name
-
+            name = " | ".join((str(user), user.nick)) if user.nick else str(user)
             if user.avatar_url:
                 info.set_author(name=name, url=user.avatar_url)
                 info.set_thumbnail(url=user.avatar_url)
@@ -69,33 +57,23 @@ class Outagetally:
                 await self.bot.say(embed=info)
             except discord.HTTPException:
                 await self.bot.say("Uh oh! I need the `Embed Links` permission to send fancy embedded messages!")
-        elif user_id not in self.settings:
-            if user_id == author.id:
-                await self.bot.reply("You are not currently in the database. You need to log your first outage before"
-                                     " I can tell you what your outage statistics are.")
+        elif user.id not in self.settings:
+            if user.id == ctx.message.author.id:
+                msg = ("You are not currently in the database. You need to log your first outage before I can tell you "
+                       "what your outage statistics are.")
             else:
-                await self.bot.reply("That user is currently not in the database. They would need to log their first"
-                                     " outage before I can display their statistics.")
-        else:
-            await self.bot.reply("Something went wrong.")  # Perfect error handling
-
-
-def check_folders():
-    tally_folder = "data/pwning-cogs/outagecounter"
-    if not os.path.exists(tally_folder):
-        print("Creating outage counter folder...")
-        os.makedirs(tally_folder)
-
-
-def check_files():
-    tally_path = "data/pwning-cogs/outagecounter/tally.json"
-    if not dataIO.is_valid_json(tally_path):
-        print("Creating empty tally.json...")
-        dataIO.save_json(tally_path, {})
+                msg = ("That user is currently not in the database. They would need to log their first outage before I "
+                       "can display their statistics.")
+            await self.bot.reply(msg)
 
 
 def setup(bot):
-    check_folders()
-    check_files()
-    n = Outagetally(bot)
-    bot.add_cog(n)
+    if not os.path.exists(PATH):
+        print("Creating outage counter folder...")
+        os.makedirs(PATH)
+
+    if not dataIO.is_valid_json(JSON):
+        print("Creating empty tally.json...")
+        dataIO.save_json(JSON, {})
+
+    bot.add_cog(Outagetally(bot))
